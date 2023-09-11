a4a24a1af3d4c1aca93ea7c12c11787f172ef74e
Oliver Delaney <oliver.delaney@stu.barren.kyschools.us>
2023-09-11 10:12:12 -0500

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Oliver Delaney on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
